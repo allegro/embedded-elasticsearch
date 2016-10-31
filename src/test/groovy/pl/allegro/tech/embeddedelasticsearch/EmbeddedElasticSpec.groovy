ca98d009bdc52fb5b6a4a55c62095b4a15c09569
Pawel Bobruk <pawel.bobruk@allegrogroup.com>
2016-10-31 21:58:16 +0100

    chg: reuse client in test

--- src/test/groovy/pl/allegro/tech/embeddedelasticsearch/EmbeddedElasticSpec.groovy
+++ src/test/groovy/pl/allegro/tech/embeddedelasticsearch/EmbeddedElasticSpec.groovy
@@ -4,10 +4,7 @@ import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequest
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse
 import org.elasticsearch.action.get.GetResponse
 import org.elasticsearch.client.Client
-import org.elasticsearch.common.settings.Settings
-import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.index.query.QueryBuilders
-import org.elasticsearch.transport.client.PreBuiltTransportClient
 import org.skyscreamer.jsonassert.JSONAssert
 import spock.lang.Shared
 import spock.lang.Specification
@@ -17,15 +14,18 @@ import static pl.allegro.tech.embeddedelasticsearch.ElasticInfo.PLUGIN_BY_NAME
 import static pl.allegro.tech.embeddedelasticsearch.ElasticInfo.PLUGIN_BY_URL
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.AMERICAN_PSYCHO
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.AUDIO_BOOK_INDEX_TYPE
+import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.AudioBook
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.BOOKS_INDEX
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.BOOKS_INDEX_NAME
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.CARS_INDEX
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.CARS_INDEX_NAME
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.CAR_INDEX_TYPE
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.CLUSTER_NAME
+import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.Car
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.FIAT_126p
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.PAPER_BOOK_INDEX_TYPE
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.PORT
+import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.PaperBook
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.SHINING
 import static pl.allegro.tech.embeddedelasticsearch.SampleIndices.toJson
 
@@ -33,6 +33,9 @@ class EmbeddedElasticSpec extends Specification {
 
     @Shared
     EmbeddedElastic embeddedElastic
+
+    @Shared
+    Client client;
 
     def setupSpec() {
         embeddedElastic = EmbeddedElastic.builder()
@@ -45,6 +48,7 @@ class EmbeddedElasticSpec extends Specification {
                 .withIndex(BOOKS_INDEX_NAME, BOOKS_INDEX)
                 .build()
                 .start()
+        client = embeddedElastic.createClient()
     }
 
     def setup() {
@@ -60,7 +64,7 @@ class EmbeddedElasticSpec extends Specification {
         index(FIAT_126p)
 
         then:
-        final result = createClient()
+        final result = client
                 .prepareSearch(CARS_INDEX_NAME)
                 .setTypes(CAR_INDEX_TYPE)
                 .setQuery(QueryBuilders.termQuery("model", FIAT_126p.model))
@@ -78,7 +82,7 @@ class EmbeddedElasticSpec extends Specification {
         embeddedElastic.index(CARS_INDEX_NAME, CAR_INDEX_TYPE, ["$id": document])
 
         then:
-        final GetResponse result = createClient()
+        final GetResponse result = client
                 .prepareGet(CARS_INDEX_NAME, CAR_INDEX_TYPE, id)
                 .execute().actionGet()
         result.exists
@@ -95,12 +99,12 @@ class EmbeddedElasticSpec extends Specification {
         embeddedElastic.recreateIndex(BOOKS_INDEX_NAME)
 
         then: "recreated index should be empty"
-        with(createClient().prepareSearch(BOOKS_INDEX_NAME).execute().actionGet()) { booksIndexSearchResult ->
+        with(client.prepareSearch(BOOKS_INDEX_NAME).execute().actionGet()) { booksIndexSearchResult ->
             booksIndexSearchResult.hits.size() == 0
         }
 
         and: "other index should be untouched"
-        with(createClient().prepareSearch(CARS_INDEX_NAME).execute().actionGet()) { carsIndexSearchResult ->
+        with(client.prepareSearch(CARS_INDEX_NAME).execute().actionGet()) { carsIndexSearchResult ->
             carsIndexSearchResult.hits.size() == 1
         }
     }
@@ -115,7 +119,7 @@ class EmbeddedElasticSpec extends Specification {
         embeddedElastic.recreateIndices()
 
         then: "recreated indices should be empty"
-        final result = createClient().prepareSearch().execute().actionGet()
+        final result = client.prepareSearch().execute().actionGet()
         result.hits.size() == 0
     }
 
@@ -125,13 +129,13 @@ class EmbeddedElasticSpec extends Specification {
         index(AMERICAN_PSYCHO)
 
         then:
-        with(createClient().prepareSearch(BOOKS_INDEX_NAME).setTypes(PAPER_BOOK_INDEX_TYPE).execute().actionGet()) { paperBooksSearchResult ->
+        with(client.prepareSearch(BOOKS_INDEX_NAME).setTypes(PAPER_BOOK_INDEX_TYPE).execute().actionGet()) { paperBooksSearchResult ->
             paperBooksSearchResult.hits.hits.size() == 1
             assertJsonsEquals(toJson(SHINING), paperBooksSearchResult.hits.hits[0].sourceAsString)
         }
 
         and:
-        with(createClient().prepareSearch(BOOKS_INDEX_NAME).setTypes(AUDIO_BOOK_INDEX_TYPE).execute().actionGet()) { audioBooksSearchResult ->
+        with(client.prepareSearch(BOOKS_INDEX_NAME).setTypes(AUDIO_BOOK_INDEX_TYPE).execute().actionGet()) { audioBooksSearchResult ->
             audioBooksSearchResult.hits.hits.size() == 1
             assertJsonsEquals(toJson(AMERICAN_PSYCHO), audioBooksSearchResult.hits.hits[0].sourceAsString)
         }
@@ -172,7 +176,7 @@ class EmbeddedElasticSpec extends Specification {
         nodesInfoRequest.plugins()
 
         when:
-        NodesInfoResponse infoResponse = createClient().admin().cluster()
+        NodesInfoResponse infoResponse = client.admin().cluster()
                 .nodesInfo(nodesInfoRequest).actionGet()
 
         then:
@@ -184,29 +188,19 @@ class EmbeddedElasticSpec extends Specification {
         plugins.contains("analysis-stempel")
     }
 
-    Client createClient() {
-        Settings settings = Settings.builder()
-                .put("cluster.name", CLUSTER_NAME)
-                .build();
-
-        Client client = new PreBuiltTransportClient(settings)
-        client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(SampleIndices.HOST), SampleIndices.PORT));
-        return client;
-    }
-
     void assertJsonsEquals(String expectedJson, String actualJson) {
         JSONAssert.assertEquals(expectedJson, actualJson, false)
     }
 
-    void index(SampleIndices.Car car) {
+    void index(Car car) {
         embeddedElastic.index(CARS_INDEX_NAME, CAR_INDEX_TYPE, toJson(car))
     }
 
-    void index(SampleIndices.PaperBook book) {
+    void index(PaperBook book) {
         embeddedElastic.index(BOOKS_INDEX_NAME, PAPER_BOOK_INDEX_TYPE, toJson(book))
     }
 
-    void index(SampleIndices.AudioBook book) {
+    void index(AudioBook book) {
         embeddedElastic.index(BOOKS_INDEX_NAME, AUDIO_BOOK_INDEX_TYPE, toJson(book))
     }
 
